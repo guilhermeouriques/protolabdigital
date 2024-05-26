2bc2852a68e2c1e6df78f047d7f1049898548af0
Guilherme Ouriques <guilherme.nro@gmail.com>
2024-05-26 16:43:20 -0300

    Versão 0.1.21

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Senai SC-ProtoLabDigital-0.1.18";
+const cacheName = "Senai SC-ProtoLabDigital-0.1.21";
 const contentToCache = [
     "Build/protolabdigital.loader.js",
     "Build/protolabdigital.framework.js.unityweb",
