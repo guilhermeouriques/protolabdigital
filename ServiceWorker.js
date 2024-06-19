b033e0dd33318b4a79e7974ca7e0112c4eb6f3a1
Guilherme Ouriques <guilherme.ouriques@sc.senai.br>
2024-06-19 09:06:45 -0300

    v0.2.2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Senai SC-ProtoLabDigital-0.2.1";
+const cacheName = "Senai SC-ProtoLabDigital-0.2.2";
 const contentToCache = [
     "Build/protolabdigital.loader.js",
     "Build/protolabdigital.framework.js.unityweb",
